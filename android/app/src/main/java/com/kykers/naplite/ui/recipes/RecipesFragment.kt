8a2e0c1ba1cc64cd1345dfcd25320a3ff41170ce
goga133 <NewC#Box1>
2020-11-02 13:56:52 +0300

    set a default template

--- android/app/src/main/java/com/kykers/naplite/ui/recipes/RecipesFragment.kt
+++ android/app/src/main/java/com/kykers/naplite/ui/recipes/RecipesFragment.kt
@@ -1,4 +1,4 @@
-package com.kykers.naplite.ui.notifications
+package com.kykers.naplite.ui.recipes
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.kykers.naplite.R
 
-class NotificationsFragment : Fragment() {
+class RecipesFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        recipesViewModel =
+                ViewModelProvider(this).get(RecipesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_recipes, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        recipesViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- android/app/src/main/java/com/kykers/naplite/ui/recipes/RecipesViewModel.kt
+++ android/app/src/main/java/com/kykers/naplite/ui/recipes/RecipesViewModel.kt
@@ -1,13 +1,13 @@
-package com.kykers.naplite.ui.home
+package com.kykers.naplite.ui.recipes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class RecipesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is recipes Fragment"
     }
     val text: LiveData<String> = _text
 }
