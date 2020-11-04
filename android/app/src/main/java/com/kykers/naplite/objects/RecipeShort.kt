789767bce82177ce35f3392228c059a1756e775c
goga133 <NewC#Box1>
2020-11-04 17:15:20 +0300

    Обновлён объект RecipeShort.kt

--- android/app/src/main/java/com/kykers/naplite/objects/RecipeShort.kt
+++ android/app/src/main/java/com/kykers/naplite/objects/RecipeShort.kt
@@ -3,6 +3,7 @@ package com.kykers.naplite.objects
 import android.os.Parcel
 import android.os.Parcelable
 import java.io.Serializable
+import java.util.*
 
 /**
  * Объект, который представляет сущность в виде кратного рецепта.
@@ -14,36 +15,46 @@ import java.io.Serializable
  */
 data class RecipeShort(
     val id: Int,
-    val url: String,
-    val title: String,
-    val imageUrl: String,
-    val description: String,
-    val pubDateTime: Int,
+    val url: String?,
+    val title: String?,
+    val imageFullUrl: String?,
+    val image150xUrl: String?,
+    val image200xUrl: String?,
+    val image300xUrl: String?,
+    val description: String?,
+    val pubDateTime: Date,
     val comments: Int,
     val favourites: Int,
     val points: Int,
-    val author: String
+    val author: String?
 ) : Serializable, Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readSerializable() as Date,
         parcel.readInt(),
         parcel.readInt(),
         parcel.readInt(),
-        parcel.readInt(),
-        parcel.readString().toString()
-    )
+        parcel.readString()
+    ) {
+    }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(id)
         parcel.writeString(url)
         parcel.writeString(title)
-        parcel.writeString(imageUrl)
+        parcel.writeString(imageFullUrl)
+        parcel.writeString(image150xUrl)
+        parcel.writeString(image200xUrl)
+        parcel.writeString(image300xUrl)
         parcel.writeString(description)
-        parcel.writeInt(pubDateTime)
+        parcel.writeSerializable(pubDateTime)
         parcel.writeInt(comments)
         parcel.writeInt(favourites)
         parcel.writeInt(points)
