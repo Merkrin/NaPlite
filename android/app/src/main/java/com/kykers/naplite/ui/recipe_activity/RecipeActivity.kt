c7f46086f50389890e16908575d8bd8447994120
goga133 <asromanyuk@edu.hse.ru>
2020-11-18 17:14:47 +0300

    viewmodel

--- android/app/src/main/java/com/kykers/naplite/ui/recipe_activity/RecipeActivity.kt
+++ android/app/src/main/java/com/kykers/naplite/ui/recipe_activity/RecipeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.kykers.naplite.R
 
 class RecipeActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class RecipeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val viewModel = ViewModelProviders.of(this).get(RecipeActivityViewModel::class.java)
+
+        viewModel.recipe.observe(viewLifecycleOwner, )
     }
 }
